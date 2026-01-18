3c6521f348b949f8aed26a265113057ac51f39e4
Kanav Gupta <kanavg004@gmail.com>
2026-01-18 07:44:57 -0500

    change

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+
+if (!process.env.ANTHROPIC_API_KEY) {
+  throw new Error("ANTHROPIC_API_KEY environment variable is required");
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
